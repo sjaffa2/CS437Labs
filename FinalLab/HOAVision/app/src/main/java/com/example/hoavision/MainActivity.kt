f6e66b979eee85b38c24f07730ea9a2dbe27cfab
Shayan Jaffar <sjaffa2@illinois.edu>
2025-11-11 14:16:25 -0600

    initial setup

---
+++ FinalLab/HOAVision/app/src/main/java/com/example/hoavision/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.hoavision
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.WindowCompat
+import com.example.hoavision.data.TfLiteLandmarkClassifier
+import com.example.hoavision.domain.Classification
+import com.example.hoavision.presentation.CameraPreview
+import com.example.hoavision.presentation.LandmarkImageAnalyzer
+import com.example.hoavision.ui.theme.HOAVisionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        if(!hasCameraPermission()) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.CAMERA), 0
+            )
+        }
+        setContent {
+            HOAVisionTheme {
+                var classifications by remember {
+                    mutableStateOf(emptyList<Classification>())
+                }
+                val analyzer = remember {
+                    LandmarkImageAnalyzer(
+                        classifier = TfLiteLandmarkClassifier(
+                            context = applicationContext
+                        ),
+                        onResults = {
+                            classifications = it
+                        }
+                    )
+                }
+                val controller = remember {
+                    LifecycleCameraController(applicationContext).apply {
+                        setEnabledUseCases(CameraController.IMAGE_ANALYSIS)
+                        setImageAnalysisAnalyzer(
+                            ContextCompat.getMainExecutor(applicationContext),
+                            analyzer
+                        )
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .windowInsetsPadding(WindowInsets.statusBars)
+                ) {
+                    CameraPreview(controller, Modifier.fillMaxSize())
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.TopCenter)
+                    ) {
+                        classifications.forEach {
+                            Text(
+                                text = it.name + " " + it.score*100 + "%",
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(MaterialTheme.colorScheme.primaryContainer)
+                                    .padding(8.dp),
+                                textAlign = TextAlign.Center,
+                                fontSize = 20.sp,
+                                color = MaterialTheme.colorScheme.primary
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun hasCameraPermission() = ContextCompat.checkSelfPermission(
+        this, Manifest.permission.CAMERA
+    ) == PackageManager.PERMISSION_GRANTED
+}
---
+++ FinalLab/HOAVision/app/src/main/java/com/example/hoavision/domain/Classification.kt
@@ -0,0 +1,6 @@
+package com.example.hoavision.domain
+
+data class Classification(
+    val name: String,
+    val score: Float
+)
